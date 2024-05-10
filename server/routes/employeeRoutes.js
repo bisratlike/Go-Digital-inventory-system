d6f45c851866cf90e3672c7e7b5f5d2c0def0386
bisratlike <bisratlike@gmail.com>
2024-05-10 15:54:04 +0300

    edition on update employee function

--- server/routes/employeeRoutes.js
+++ server/routes/employeeRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const employeeController = require("../controllers/employeeController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.post("/createEmployee", authMiddleware.authorize("manager"||"ceo"), employeeController.createEmployee);
+router.post("/createEmployee",employeeController.createEmployee);
 router.get("/allEmployee", authMiddleware.authorize("manager"||"ceo"), employeeController.getAllEmployees);
 router.put("/updateEmployee/:id",employeeController.updateEmployee);
 module.exports = router;
