8545e5a6e0e9281f6c533b40f2523e1a63b4088a
bisratlike <bisratlike@gmail.com>
2024-05-11 10:28:31 +0300

    get one employee

--- server/routes/employeeRoutes.js
+++ server/routes/employeeRoutes.js
@@ -6,4 +6,5 @@ const authMiddleware = require("../middleware/authMiddleware");
 router.post("/createEmployee",authMiddleware.authorize("manager"||"ceo"),employeeController.createEmployee);
 router.get("/allEmployee", authMiddleware.authorize("manager"||"ceo"), employeeController.getAllEmployees);
 router.put("/updateEmployee/:id",authMiddleware.authorize("manager"||"ceo"),employeeController.updateEmployee);
+router.get("/oneEmployee",employeeController.getOneEmployee);
 module.exports = router;
